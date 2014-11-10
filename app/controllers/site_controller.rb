413f521cacd86ae24df6e9199e1a446c576e6c9d
dianaberenguer <dianaberenguer@gmail.com>
2014-11-10 14:18:27 +0000

    site controller

---
+++ app/controllers/site_controller.rb
@@ -0,0 +1,9 @@
+class SiteController < ApplicationController
+	def home
+		# render 'home'
+	end
+
+	def contact
+		# render 'contact' - no hace falta ponerlo pq por defecto coge la ruta del nombre de la acción
+	end
+end
